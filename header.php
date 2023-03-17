7f278759199b9306311a1b8494dff84e4b870531
ben@litliving.com <bsmith20681@gmail.com>
2023-03-17 12:13:03 -0600

    second commit

---
+++ header.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;800&display=swap"
+      rel="stylesheet"
+    />
+    <link
+      rel="stylesheet"
+      type="text/css"
+      href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.css"
+      integrity="sha512-6lLUdeQ5uheMFbWm3CP271l14RsX1xtx+J5x2yeIDkkiBpeVTNhTqijME7GgRKKi6hCqovwCoBTlRBEC20M8Mg=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+    />
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class( 'bg-white text-gray-900 antialiased' ); ?>>
+
+<?php do_action( 'tailpress_site_before' ); ?>
+
+<div id="page" class="min-h-screen flex flex-col">
+
+	<?php do_action( 'tailpress_header' ); ?>
+
+	<header class="bg-dark-blue">
+
+		<div class="container">
+			<div class="lg:flex lg:justify-between lg:items-center py-6">
+				<div class="flex justify-between items-center">
+					<div>
+						<?php if ( has_custom_logo() ) { ?>
+                            <?php the_custom_logo(); ?>
+						<?php } else { ?>
+							<a href="<?php echo get_bloginfo( 'url' ); ?>" class="text-white text-lg uppercase">
+								<?php echo get_bloginfo( 'name' ); ?>
+							</a>
+
+							<p class="text-sm font-light text-gray-600">
+								<?php echo get_bloginfo( 'description' ); ?>
+							</p>
+
+						<?php } ?>
+					</div>
+
+		
+				</div>
+
+				<?php
+					wp_nav_menu(
+						array(
+							'container_id'    => 'primary-menu',
+							'container_class' => 'hidden bg-gray-100 mt-4 p-4 lg:mt-0 lg:p-0 lg:bg-transparent lg:block',
+							'menu_class'      => 'lg:flex lg:-mx-4',
+							'theme_location'  => 'primary',
+							'li_class'        => 'lg:mx-4 text-white',
+							'a_class'         => 'flex',
+							'fallback_cb'     => false,
+						)
+					);
+				?>
+			</div>
+		</div>
+	</header>
+
+
+
+	
+
